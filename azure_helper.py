5886d471a0005c7e9ce1b13f7675865c6b0027a6
crypt0sploit <aady81@outlook.com>
2022-05-29 18:36:10 +0300

    Last changes

--- azure_helper.py
+++ azure_helper.py
@@ -9,8 +9,7 @@ from datetime import datetime
 from azure.cli.core import get_default_cli
 import subprocess
 import argparse
-import pandas as pd
-# import parser
+# import pandas as pd
 
 class bcolors:
     HEADER = '\033[95m'
@@ -27,16 +26,20 @@ class main_parser:
     def __init__(self):
         
         # Arguments for usage and options
+
         self.parser = argparse.ArgumentParser(description="Azure CLI Helper", add_help=True)
         self.parser.add_argument("-l", "--login", help="Login into a Microsoft Azure Account",\
                                             action="store_true")
-        self.parser.add_argument("-s", "--subs", help="List the subscriptions of the Azure Account as a tsv file",\
+        self.parser.add_argument("-s", "--subs", help="List the subscriptions of the Azure Account in an output file",\
                                             action="store_true")
         self.parser.add_argument("-a", "--alerts", help="List all alerts in a file",\
                                             action="store_true")
         self.parser.add_argument("-r", "--recom", help="List security recommendations in an output file.",\
                                             action="store_true")
-        self.parser.add_argument("output", nargs="?", type=str, default="json", help="Specify the output file type")
+        self.parser.add_argument("-c", "--checkazure", help="Check if azure is existent, if not install it.",\
+                                            action="store_true")
+        self.parser.add_argument("output", nargs="?", type=str, default="json", help="Specify the output file type [json] [tsv] [table] [yaml]")
+        #self.parser.add_argument("-v", "--verbose", nargs="?", type=str, help="Show the output in terminal")
         self.args = self.parser.parse_args()
 
 class azure_defender:
@@ -45,16 +48,24 @@ class azure_defender:
         self.az_cli = get_default_cli()
         self.date = datetime.now().strftime("%Y%m%d-%H%M%S")
 
+    # Login to azure account function 
+
     def login(self):
         print(self.az_cli.invoke(['login']))
+
         try:
             print(f"{bcolors.OKGREEN}\nOK: Logged in successfully\n")
         except KeyboardInterrupt:
             print(f"{bcolors.WARNING}WARNING: Aborted.")
 
     def list_subs(self):
+
+        # Check if Folder is existent
+
         if os.path.isdir("Subscriptions") == False:
             os.mkdir("Subscriptions")
+
+        # File type condition
 
         if main_parser().args.output == "table":
             subprocess.check_output([f'az account list --output {main_parser().args.output} > Subscriptions/subscriptions_{self.date}.tsv'], shell=True)
@@ -68,8 +79,13 @@ class azure_defender:
 
 
     def list_alerts(self):
+
+        # Check if Folder is existent
+
         if os.path.isdir("Alerts") == False:
             os.mkdir("Alerts")
+
+        # File type condition
 
         if main_parser().args.output == "table":
             subprocess.check_output([f'az security alert list --output {main_parser().args.output} > Alerts/alerts_{self.date}.tsv'], shell=True)
@@ -82,8 +98,13 @@ class azure_defender:
             print(f"{bcolors.FAIL}{bcolors.BOLD}INVALID FORMAT")
 
     def list_recommendations(self):
+
+        # Check if Folder is existent
+
         if os.path.isdir("Recommendations") == False:
             os.mkdir("Recommendations")
+
+        # File type condition
 
         if main_parser().args.output == "table":
             subprocess.check_output([f"az security assessment list --output {main_parser().args.output} > Recommendations/recommendations_{self.date}.tsv"], shell=True)
@@ -94,7 +115,17 @@ class azure_defender:
             print(f"{bcolors.OKCYAN}\nOK: Listed the security recommendations on each subscription.\n")
         else:
             print(f"{bcolors.FAIL}{bcolors.BOLD}INVALID FORMAT")
- 
+
+    def check_for_azure(self):
+        try:
+            azure_check = subprocess.call(["az"])
+        except FileNotFoundError:
+            print(f"{bcolors.BOLD}{bcolors.WARNING}\nAzure CLI not found...\n")
+            choice = input("Do you want to install it? (y/n) > ")
+            if choice == "y":
+                subprocess.call(["powershell","-Command","$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\\AzureCLI.msi"], shell=True, stdout=sys.stdout)
+            else:
+                print(f"{bcolors.BOLD}{bcolors.WARNING}\nAborted!\n")
 
 def main():
     execute = azure_defender()
@@ -111,7 +142,9 @@ def main():
     
     if argparser.args.recom:
         execute.list_recommendations()
-
+    
+    if argparser.args.checkazure:
+        execute.check_for_azure()
     # execute = azure_defender()
     # execute.list_subs()
 
